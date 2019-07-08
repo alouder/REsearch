8020b3b64c9c460f90b9776e7631861f5f100df4
alouder <alouder17@gmail.com>
2019-07-08 13:15:25 -0400

    Update REsearch.py and scrape.py

--- src/REsearch.py
+++ src/REsearch.py
@@ -1,4 +1,5 @@
 from itertools import product
+import re
 import test
 import scrape
 
@@ -29,37 +30,127 @@ amino_acid_codons = {
 	"stop": ["TAA", "TAG", "TGA"]
 }
 
+# Dictionary to store all bases that correspond to symbols that represent ambiguity
+ambiguity = {
+	"G": ["G"],
+	"C": ["C"],
+	"A": ["A"],
+	"T": ["T"],
+	"R": ["G", "A"],
+	"Y": ["C", "T"],
+	"M": ["A", "C"],
+ 	"K": ["G", "T"],
+	"S": ["G", "C"],
+	"W": ["A", "T"],
+	"B": ["C", "G", "T"],
+	"D": ["A", "G", "T"],
+	"H": ["A", "C", "T"],
+	"V": ["A", "C", "G"],
+	"N": ["N"] #["A", "C", "G", "T"]
+}
+
 # Find the number of possible codon combinations for a particular amino acid sequence
-def num_combinations(amino_acid_sequence):
+# Return integer
+def numCombinations(amino_acid_sequence):
 	comb = 1
 	for i in amino_acid_sequence:
 		comb *= len(amino_acid_codons[i])
 	return comb
 
-# Build all of the possible codon combinations for an amino acid sequence using itertools.
-# Returns a list of tuples which contain each combination of codons. 
-def build_possible_sequences(amino_acid_sequence):
+# Build all possible combinations for a sequence using itertools.
+# Returns a list of tuples which contain each combination 
+def buildPossibleSequences(sequence, dic):
     codon_comb = []
-    for i in amino_acid_sequence:
-    	codon_comb.append(amino_acid_codons[i])
+    for i in sequence:
+    	codon_comb.append(dic[i])
 
     return list(product(*codon_comb))
-    
+
 # Join the tuples created by build_possible_sequences and return a list of possible RNA sequences (codon combinations) as strings
-def join_tuples_list(join_string, tuple_list):
-	codon_comb_string = []
-	for i in tuple_list:
+def joinTuplesList(join_string, lst):
+	codon_comb_list = []
+	for i in lst:
 		s = join_string.join(i)
-		codon_comb_string.append(s)
-	return codon_comb_string
+		codon_comb_list.append(s)
+	return codon_comb_list
+
+# Distinguish palindromic from non-palindromic enzymes (as indicated by the NEB notation)
+# Return boolean
+def isPalindromic(seq):
+	return "(" in seq
+
+# Filter numbers and parentheses out of non-palindromic enzymes. 
+# Return sanitized string.
+def splitNonPalindromic(seq):
+	if re.search("^\(.*\)", seq) is not None:
+		temp_list = re.split("\)", seq, 1)
+		if re.search("\(.*\)", temp_list[1]) is not None:
+			return re.split("\(", temp_list[1])[0]
+		else:
+			return temp_list[1]
+	return re.split("\(", seq)[0]
+
+# UNFINISHED
+def matchSequence(search_string, match_string):
+	search_N = re.search("N", match_string)
+	if search_N is not None:
+		index_of_Ns = []
+		for i in range(len(match_string)):
+			if match_string[i] == "N":
+				index_of_Ns.append(i)
+
+# Eliminate certain enzymes based on whether or not a given base is present in the amino acid sequence	
+def eliminateEnzymeByBase(codon_list, enz_dict, base):
+	base_present = False
+	i = 0
+	while i < len(codon_list) and base_present == False:
+		if base in codon_list[i]:
+			base_present = True
+		else:
+			i += 1
+	if base_present == False:
+		for i in enz_dict:
+			for j in enz_dict[i]:
+				if base in j:
+					enz_dict[i].remove(j)
+
+# Run eliminateEnzymeByBase for all bases
+def checkAllBases(codon_list, enz_dict):
+	eliminateEnzymeByBase(codon_list, enz_dict, "G")
+	eliminateEnzymeByBase(codon_list, enz_dict, "C")
+	eliminateEnzymeByBase(codon_list, enz_dict, "A")
+	eliminateEnzymeByBase(codon_list, enz_dict, "T")
+
+# Eliminate certain enzymes by the length of the amino acid sequence inputted
+def eliminateEnzymeByLength(enz_dict, amino_acid_sequence):
+	max_len = len(amino_acid_sequence) * 3
+	for i in enz_dict:
+		for j in enz_dict[i]:
+			if max_len > len(j):
+				enz_dict[i].remove(j)
 
 # Driver code
-
 test_amino_acid_sequence = input("Enter amino acid squence: ")
 
-codon_comb_list = join_tuples_list("", build_possible_sequences(test_amino_acid_sequence))
+codon_comb_list = joinTuplesList("", buildPossibleSequences(test_amino_acid_sequence, amino_acid_codons))
 
-cnt = 1
-for i in codon_comb_list:
-	print("%2d. %s" %(cnt, i))
-	cnt += 1	
+# Establish var from restriction enzyme dictionary in scrape.py (###TO BE MODULARIZED###)
+neb_enz_seq_dict = scrape.neb_enz_seq_dict
+
+# Modified dictionaries to hold the RE recognition sequences (and corresponding REs) with out cleavage site indicator (for processing).
+# Palindromic and non-palindromic enzymes are distinguished.
+mod_enz_seqs = {}
+for i in neb_enz_seq_dict:
+	if not isPalindromic(neb_enz_seq_dict[i]):
+		mod = re.sub("/", "", neb_enz_seq_dict[i])
+		mod_enz_seqs[i] = mod
+	else:
+		mod = splitNonPalindromic(neb_enz_seq_dict[i])
+		mod_enz_seqs[i] = mod
+
+absolute_mod_enz_seqs = {}
+for i in mod_enz_seqs:
+	absolute_mod_enz_seqs[i] = joinTuplesList("", buildPossibleSequences(mod_enz_seqs[i], ambiguity))
+
+# Final dictionary holding possible enzymes and codon sequences
+for i in 
--- src/scrape.py
+++ src/scrape.py
@@ -8,30 +8,30 @@ def multipleA(parent):
 		return True
 	return False
 
-def tagListToDict(parent_list, index, tag, dic):
-	for i in parent_list[index].find_all(tag):
-		dic[i.string] = parent_list[index - 1].string
+def openUrlConnection(url):
+	req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+	uClient = urlopen(req)
+	html_page = uClient.read()
+	uClient.close()
+	return html_page
 
 
 # Store open connection to URL into var, store html page into var, and then close connection
 neb_url = "https://www.neb.com/tools-and-resources/selection-charts/alphabetized-list-of-recognition-specificities"
-req = Request(neb_url, headers={'User-Agent': 'Mozilla/5.0'})
-uClient = urlopen(req)
-neb_initial_html = uClient.read()
-uClient.close()
+neb_initial_html = openUrlConnection(neb_url)
 
 # Store html parse tree into var using BeautifulSoup
 neb_initial_soup = soup(neb_initial_html, "html.parser")
-total_list = neb_initial_soup.table.find_all("td")
+neb_total_list = neb_initial_soup.table.find_all("td")
 
-enz_seq_dict = {}
-for i in range(0,len(total_list),2):
-	if multipleA(total_list[i + 1]):
-		# DECIDE TO KEEP: tagListToDict(total_list, i + 1, "a", enz_seq_dict)
-		for j in total_list[i + 1].find_all('a'):
-			enz_seq_dict[j.string] = total_list[i].string
+# MODULARIZE
+neb_enz_seq_dict = {}
+for i in range(0,len(neb_total_list),2):
+	if multipleA(neb_total_list[i + 1]):
+		for j in neb_total_list[i + 1].find_all('a'):
+			neb_enz_seq_dict[j.string] = neb_total_list[i].string
 	else:
-		enz_seq_dict[total_list[i + 1].a.string] = total_list[i].string
+		neb_enz_seq_dict[neb_total_list[i + 1].a.string] = neb_total_list[i].string
 
 
 
